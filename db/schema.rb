9fd17a9c18bc1a4fbb9314a36b09a528cabe5084
af <a.fabiano@live.fr>
2017-09-05 10:50:20 +0200

    Add Controller Universes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904154608) do
+ActiveRecord::Schema.define(version: 20170905083943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pages", force: :cascade do |t|
+    t.string "content"
+    t.integer "position"
+    t.bigint "story_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["story_id"], name: "index_pages_on_story_id"
+  end
+
+  create_table "solutions", force: :cascade do |t|
+    t.bigint "story_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["story_id"], name: "index_solutions_on_story_id"
+    t.index ["user_id"], name: "index_solutions_on_user_id"
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.bigint "universe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["universe_id"], name: "index_stories_on_universe_id"
+  end
+
+  create_table "universes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +65,8 @@ ActiveRecord::Schema.define(version: 20170904154608) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pages", "stories"
+  add_foreign_key "solutions", "stories"
+  add_foreign_key "solutions", "users"
+  add_foreign_key "stories", "universes"
 end

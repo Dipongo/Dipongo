e12f09ed3cbc394fda115a963881b23ac3c9007c
etienne ziegelmeyer <ziegelmeyer.etienne@gmail.com>
2017-09-05 19:11:32 +0200

    Changed routes

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,7 +1,11 @@
 class SolutionsController < ApplicationController
 
+  skip_before_action :authentificat_user!, only: [:index, :new]
+
+  before_action :set_story, only: [:index, :create]
+
   def index
-    @solutions = Solution.all
+    @story = Solution.all
   end
 
   def new
@@ -10,7 +14,14 @@ class SolutionsController < ApplicationController
 
   def create
     @solution = current_user.solutions.new(params[:solution])
+
     @solution.save
   end
+
+  private
+
+    def set_story
+      @stories = Story.find(params[:story_id])
+    end
 
 end

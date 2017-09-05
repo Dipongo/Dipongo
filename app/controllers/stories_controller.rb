6fd59f1fe9c8133caa1157de4639e8b526c08657
af <a.fabiano@live.fr>
2017-09-05 15:25:19 +0200

    Controller Stories done

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,21 @@
 class StoriesController < ApplicationController
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    @story.universe = Universe.find(params[:universe_id])
+    @story.save
+  end
+
   def show
-    @story = Story.pages
+    @story = Story.find(params[:id])
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:name, :position)
   end
 end

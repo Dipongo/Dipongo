578a5d5a4393e0bc4161f6aa2b6799025b2d49a1
loutrusmagnus <cyrilroussy@gmail.com>
2017-10-05 12:20:50 +0200

    CSS fond story et div story text

--- app/admin/stories.rb
+++ app/admin/stories.rb
@@ -2,7 +2,15 @@ ActiveAdmin.register Story do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :universe_id, :name
+permit_params :universe_id, :name, :photo
+
+  form do |f|
+    f.inputs "Nouvelle histoire" do
+      f.input :name
+      f.input :photo, as: :formtastic_attachinary ### THERE ####
+    end
+    actions
+  end
 #
 # or
 #
